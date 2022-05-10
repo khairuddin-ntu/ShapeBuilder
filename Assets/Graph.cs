0e1d1351142bdcf52b5930eb26145367aa3709e7
khairuddin-ntu <khai0050@e.ntu.edu.sg>
2022-05-10 14:09:36 +0800

    Set minimum & maximum zoom levels

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -6,6 +6,8 @@ public class Graph : MonoBehaviour
 {
     private static float ROTATION_SPEED = 7.0f;
     private static float SCALE_SPEED = 0.2f;
+    private static Vector3 MAX_SCALE = new Vector3(7f, 7f, 7f);
+    private static Vector3 MIN_SCALE = new Vector3(1f, 1f, 1f);
 
     void Start() {}
 
@@ -14,6 +16,11 @@ public class Graph : MonoBehaviour
         if (mouseScroll != 0f) {
             float scale = mouseScroll * SCALE_SPEED;
             transform.localScale += new Vector3(scale, scale, scale);
+            if (transform.localScale.x > MAX_SCALE.x) {
+                transform.localScale = MAX_SCALE;
+            } else if (transform.localScale.x < MIN_SCALE.x) {
+                transform.localScale = MIN_SCALE;
+            }
         }
 
     }
