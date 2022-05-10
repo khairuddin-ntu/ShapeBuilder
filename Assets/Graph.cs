9bb7e6fce35413e5bae0cfa5b113db8cb52cfa79
khairuddin-ntu <khai0050@e.ntu.edu.sg>
2022-05-10 13:56:29 +0800

    Enabled scroll to zoom into graph

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     private static float ROTATION_SPEED = 7.0f;
+    private static float SCALE_SPEED = 0.2f;
 
     void Start() {}
 
-    void Update() {}
+    void Update() {
+        float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (mouseScroll != 0f) {
+            float scale = mouseScroll * SCALE_SPEED;
+            transform.localScale += new Vector3(scale, scale, scale);
+        }
+
+    }
 
     public void OnMouseDrag() {
         float rotateX = Input.GetAxis("Mouse X") * ROTATION_SPEED;
