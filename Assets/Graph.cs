6bf89576ab31e7b40a24a721862784e22fc04fd7
khairuddin-ntu <khai0050@e.ntu.edu.sg>
2022-05-10 14:33:33 +0800

    Moved box collider to viewport on UI canvas

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -9,17 +9,19 @@ public class Graph : MonoBehaviour
     private static Vector3 MAX_SCALE = new Vector3(10f, 10f, 10f);
     private static Vector3 MIN_SCALE = new Vector3(1f, 1f, 1f);
 
+    public Transform graphTransform;
+
     void Start() {}
 
     void Update() {
         float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
         if (mouseScroll != 0f) {
             float scale = mouseScroll * SCALE_SPEED;
-            transform.localScale += new Vector3(scale, scale, scale);
-            if (transform.localScale.x > MAX_SCALE.x) {
-                transform.localScale = MAX_SCALE;
-            } else if (transform.localScale.x < MIN_SCALE.x) {
-                transform.localScale = MIN_SCALE;
+            graphTransform.localScale += new Vector3(scale, scale, scale);
+            if (graphTransform.localScale.x > MAX_SCALE.x) {
+                graphTransform.localScale = MAX_SCALE;
+            } else if (graphTransform.localScale.x < MIN_SCALE.x) {
+                graphTransform.localScale = MIN_SCALE;
             }
         }
 
@@ -28,6 +30,6 @@ public class Graph : MonoBehaviour
     public void OnMouseDrag() {
         float rotateX = Input.GetAxis("Mouse X") * ROTATION_SPEED;
 		float rotateY = Input.GetAxis("Mouse Y") * ROTATION_SPEED;
-        transform.Rotate(rotateY, -rotateX, 0.0f);
+        graphTransform.Rotate(rotateY, -rotateX, 0.0f);
     }
 }
