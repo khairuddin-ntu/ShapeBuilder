8d687b82d9bf08a005bf4616b106e20072cd4c7f
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-06-22 15:29:47 +0800

    Created methods for generating the different kinds of shapes

--- Assets/ShapeGenerator.cs
+++ Assets/ShapeGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class ShapeGenerator : MonoBehaviour
 {
     private const string TAG = "ShapeGenerator";
+    private const int SLIDER_2D_VALUE = 0;
+    private const int SLIDER_3D_VALUE = 1;
 
     public Slider slider;
 
@@ -14,5 +16,24 @@ public class ShapeGenerator : MonoBehaviour
     public void GenerateShape()
     {
         Debug.Log($"{TAG}: GenerateShape: Slider value = {slider.value}");
+        switch (slider.value)
+        {
+            case SLIDER_2D_VALUE:
+                Generate2dShape();
+                break;
+            case SLIDER_3D_VALUE:
+                Generate3dShape();
+                break;
+        }
+    }
+
+    private void Generate2dShape()
+    {
+        Debug.Log($"{TAG}: GenerateShape: Should generate 2D shape");
+    }
+
+    private void Generate3dShape()
+    {
+        Debug.Log($"{TAG}: GenerateShape: Should generate 3D shape");
     }
 }
