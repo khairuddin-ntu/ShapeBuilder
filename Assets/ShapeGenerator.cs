4b9144c99d70afd47e4c0fa1c50eb1083ed2a99f
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-06-22 15:09:49 +0800

    Mapped generate shape button click to method in ShapeGenerator

--- Assets/ShapeGenerator.cs
+++ Assets/ShapeGenerator.cs
@@ -3,9 +3,16 @@ using UnityEngine.UI;
 
 public class ShapeGenerator : MonoBehaviour
 {
+    private const string TAG = "ShapeGenerator";
+
     public Slider slider;
 
     private void Start() { }
 
     private void Update() { }
+
+    public void GenerateShape()
+    {
+        Debug.Log($"{TAG}: GenerateShape: Slider value = {slider.value}");
+    }
 }
