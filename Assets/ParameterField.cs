f1387987273d1c18d176ecd036003b8f1a6e2570
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-07-20 21:24:14 +0800

    Stored min & max value changes

--- Assets/ParameterField.cs
+++ Assets/ParameterField.cs
@@ -6,6 +6,7 @@ public class ParameterField : MonoBehaviour
     public TMP_Text paramLabel;
 
     public string paramName;
+    private int minValue = 0, maxValue = 1;
 
     void Start()
     {
@@ -21,5 +22,29 @@ public class ParameterField : MonoBehaviour
     {
         this.paramName = paramName;
         paramLabel.text = $"{paramName} = [";
+    }
+
+    public void OnMinValueChange(string minFieldValue)
+    {
+        Debug.Log($"OnMinValueChange: Field value = {minFieldValue}");
+
+        if (string.IsNullOrEmpty(minFieldValue) || !int.TryParse(minFieldValue, out minValue))
+        {
+            minValue = 0;
+        }
+
+        Debug.Log($"OnMinValueChange: Value changed to {minValue}");
+    }
+
+    public void OnMaxValueChange(string maxFieldValue)
+    {
+        Debug.Log($"OnMaxValueChange: Field value = {maxFieldValue}");
+
+        if (string.IsNullOrEmpty(maxFieldValue) || !int.TryParse(maxFieldValue, out maxValue))
+        {
+            maxValue = 1;
+        }
+
+        Debug.Log($"OnMaxValueChange: Value changed to {maxValue}");
     }
 }
