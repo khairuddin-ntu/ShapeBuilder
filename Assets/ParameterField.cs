71487347eaaac1fc8bd40cdde62ed921a42c17ca
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-07-20 20:40:24 +0800

    Attached script to GameObject; set parameter label based on parameter name

--- Assets/ParameterField.cs
+++ Assets/ParameterField.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class ParameterField : MonoBehaviour
 {
+    public TMP_Text paramLabel;
+
+    public string paramName;
+
     void Start()
     {
-        
+        paramLabel.text = $"{paramName} = [";
     }
 
     void Update()
