a297af4bdaff972abea9f153bc8677065521a42b
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-07-20 22:10:34 +0800

    Created script for managing list of parameters

---
+++ Assets/ParameterSection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterSection : MonoBehaviour, Validator
+{
+    public List<ParameterField> fields;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public ValidationResult ValidateInputs()
+    {
+        ValidationResult result;
+        foreach (Validator validator in fields)
+        {
+            result = validator.ValidateInputs();
+            if (result is not Success)
+            {
+                return result;
+            }
+        }
+
+        return new Success();
+    }
+}
